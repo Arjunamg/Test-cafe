1724b667be9740dc596c952177040263faaaf180
DineshKumar-G <dinesh.k@access-designs.com>
2019-05-22 11:28:10 +0530

    Move till FM and Movement stage handiling

--- Specs/main.spec.js
+++ Specs/main.spec.js
@@ -31,17 +31,34 @@ fixture`Material Manager`
   });
 
 test('Material', async t => {
+  /*Material Card creation with:
+    1. Order Level Dates and Level and zone
+    2. 5 items, each with FM required and item notes
+    3. Checklist for order*/
   await createMaterial.createMaterial(name);
   await common.search(name);
+  //Function for splitting the order
   await common.split(`Split-of-${name}`);
   await common.clearSearch();
   await common.search(name);
+  //Function for Cloning the Order
   await common.clone();
   await common.search(`Clone Of ${name}`);
   await common.clearSearch();
+  //Function for Combining Order
   await common.combine(`${name}`,`Split-of-${name}`);
-  await common.split(`Split-of-${name}`);
+  await common.search(`Clone Of ${name}`);
+  //Function For Removing the material order
   await common.remove();
+  await common.clearSearch();
+  await common.search(name);
+  await common.move('forward','FM');
+  await common.clearSearch();
+  await t.navigateTo('https://app.manufacton.com/#/materials/qa');
+  await common.search(name);
+  
+
+
 })
 
 
