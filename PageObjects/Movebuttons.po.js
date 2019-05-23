75320708b4aefd4106a05009484c8225c79ad140
DineshKumar-G <dinesh.k@access-designs.com>
2019-05-23 16:17:32 +0530

    Move Done Accross the Soln.

--- PageObjects/Movebuttons.po.js
+++ PageObjects/Movebuttons.po.js
@@ -12,9 +12,16 @@ export default class Movement {
         this.stagePrep =Selector('span').withText('Preparation').nth(1)
         this.stageFM =Selector('span').withText('Field Measurement').nth(1)
         this.stageOrdering =Selector('span').withText('Ordering').nth(1)
-        //.nth(43)
-
-
+        this.PoName = Selector('div').withText('Name').nth(25).find('.input')
+        this.poToPrefabConfirm = Selector('button').withText('Yes')
+        this.poToDetailingDateSelect =  Selector('.control.has-icons-left.is-clearfix').find('.input')
+        this.poToDetailingDate = Selector('.datepicker-row').nth(32).find('a').withText('15')
+        this.poToDetailingLocSelect = Selector('.multiselect').nth(8).find('div').withText('Select One')
+        this.poToDetailingLoc = Selector('li').withText('Name').nth(13).find('.multiselect__option.multiselect__option--highlight')
+        this.DetailingToManufDateSelect = Selector('.control.has-icons-left.is-clearfix').find('.input')
+        this.DetailingToManufDate = Selector('.datepicker-row').nth(62).find('a').withText('17')
+        this.manufToQaDateSelect = Selector('.control.has-icons-left.is-clearfix').find('.input')
+        this.manufToQaDate = Selector('.datepicker-row').nth(62).find('a').withText('17')
         //Below are the Shipping selectors which might be useful later.
         // this.shipButton = Selector('.fa.fa-truck.fa-2x.fa-1x.fa');
         // this.shipItemButton = Selector('.fas.m-r-5.fa.fa-truck');
@@ -27,70 +34,104 @@ export default class Movement {
         // this.shipCreate = Selector('button').withText('Create');
     }
 
-    async movement(kind,stage){
+    //Material Movement is ONLY from P->FM->O or O->FM->P
+    async movement(kind,stage,poName){
         if(kind==='forward'){
-            await this.forward(stage);
+            await this.forward(stage,poName);
         }
         else if(kind==='backward'){
             await this.backward(stage);
         }
     }
-    async forward(stage){
-        if(stage==='Prep'){
-        await t  
-            .click(this.forwardPaperPlane)
-            .click(this.forwardItemPaperPlane)
-            .click(this.stageDD)
-            .click(this.stagePrep)
-            .click(this.selectAllItems)
-            .click(this.modalMove)
-        }
-        else if(stage==='FM'){
+    //Pass Detination names as stage name
+    async forward(stage,poName){
+        if(stage==='FM'){
+            await forwardMove();
             await t  
-            .click(this.forwardPaperPlane)
-            .click(this.forwardItemPaperPlane)
-            .click(this.stageDD)
-            .click(this.stageFM)
-            .click(this.selectAllItems)
-            .click(this.modalMove)
+                .click(this.stageDD)
+                .click(this.stageFM);
+            await selectAllMove();
         }
         else if(stage==='Ordering'){
-            await t  
-            .click(this.forwardPaperPlane)
-            .click(this.forwardItemPaperPlane)
-            // .click(this.stageDD)
-            // .click(this.stageOrdering)
-            .click(this.selectAllItems)
-            .click(this.modalMove)
-            }
+            await forwardMove();
+            await selectAllMove();
+        }
+        else if(stage === 'PO'){
+            await forwardMove();
+            await t 
+                .click(this.PoName)
+                .typeText(this.PoName, poName)
+            await selectAllMove();
+        }
+        else if(stage === 'Detailing'){
+            await forwardMove();
+            await t 
+                .click(this.poToDetailingDateSelect)
+                .click(this.poToDetailingDate)
+                .click(this.poToDetailingLocSelect)
+                .click(this.poToDetailingLoc)
+            await selectAllMove();
+        }
+        else if(stage === 'Manufacturing'){
+            await forwardMove();
+            await t 
+                .click(this.DetailingToManufDateSelect)
+                .click(this.DetailingToManufDate)
+            await selectAllMove();
+        }
+        else if(stage ==='QA'){
+            await forwardMove();    
+            await t
+                .click(this.DetailingToManufDateSelect)
+                .click(this.manufToQaDate)
+            await selectAllMove();
+        }
     }
     async backward(){
-        if(stage==='Prep'){
+        if(stage==='Preparation'){
+            await backwardMove(); 
+            await selectAllMove();        
+        }
+        else if(stage==='FM'){
+            await backwardMove(); 
             await t  
-            .click(this.backwardPaperPlane)
-            .click(this.backwardItemPaperPlane)
-            .click(this.stageDD)
-            .click(this.stagePrep)
+                .click(this.stageDD)
+                .click(this.stageFM)
+            await selectAllMove();
+        }
+        else if(stage==='Prefab'){
+            await backwardMove(); 
+            await t
+                .click(this.poToPrefabConfirm)
+        }
+        else if(stage==='PO'){
+            await backwardMove(); 
+            await t 
+                .click(this.modalMove)
+        }
+        else if(stage==='Detailing'){
+            await backwardMove(); 
+            await t
+                .click(this.modalMove)
+        }
+        else if(stage==='Manufacturing'){
+            await backwardMove(); 
+            await selectAllMove();
+        }
+    }
+    async selectAllMove(){
+        await t 
             .click(this.selectAllItems)
             .click(this.modalMove)
-        }
-        if(stage==='FM'){
-            await t  
+    }
+    async forwardMove(){
+        await t
+            .click(this.forwardPaperPlane)
+            .click(this.forwardItemPaperPlane)
+    }
+    async backwardMove(){
+        await t
             .click(this.backwardPaperPlane)
             .click(this.backwardItemPaperPlane)
-            .click(this.stageDD)
-            .click(this.stageFM)
-            .click(this.selectAllItems)
-            .click(this.modalMove)
-        }
-        if(stage==='Ordering'){
-            await t  
-            .click(this.backwardPaperPlane)
-            .click(this.backwardItemPaperPlane)
-            .click(this.stageDD)
-            .click(this.stageOrdering)
-            .click(this.selectAllItems)
-            .click(this.modalMove)
-        }
     }
 }
