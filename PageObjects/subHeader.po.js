eff8fde4ab7492cc836f9983f4e7e91d1c1c85a0
SnehaGBhat24 <sneha.g.bhat515@gmail.com>
2019-05-27 12:34:11 +0530

    Creating Prefab

--- PageObjects/subHeader.po.js
+++ PageObjects/subHeader.po.js
@@ -12,9 +12,12 @@ export default class Create {
         this.zone = Selector('.column:nth-child(3)').find('div').withText('Zone').find('input')
         this.location = Selector('.multiselect__tags').nth(3).find('span').withText('Select option')
         this.recipient = Selector('.multiselect__tags').nth(4).find('span').withText('Select option')
+        this.coordBy = Selector('.control.has-icons-left.is-clearfix').find('.input')
+        this.prefabOnsite = Selector('div').withText('OnSite').nth(6).find('.input')
     }
     async create(orderName, orderBy, availBy, ship, onsite, level, zone) {
-        await t
+            const date = VueSelector('MfDate BDatepickerTable ')
+           await t
             .typeText(this.orderName, orderName)
             .click(this.orderBy)
             .click(Selector('.datepicker-row').nth(1).find('a').withText(orderBy))
@@ -27,8 +30,16 @@ export default class Create {
             .typeText(this.level, level)
             .typeText(this.zone, zone)
             .click(this.location)
-            .click(Selector('li').withText('Loc Name'))
+            .click(Selector('li').withText('lak loc2'))
             .click(this.recipient)
-            .click(Selector('.multiselect__option.multiselect__option--highlight').nth(4).find('span').withText('UI'))
+            .click(Selector('.multiselect__option.multiselect__option--highlight').nth(4).find('span').withText('Anil Kumar'))
+    }
+    async creatPrefab(orderName ,coordBy, prefabOnsite){
+        await t
+            .typeText(this.orderName, orderName)
+            .click(this.coordBy)
+            .click(Selector('.datepicker-row').nth(1).find('a').withText(coordBy))
+            .click(this.prefabOnsite)
+            .click(Selector('.datepicker-row').nth(9).find('a').withText(prefabOnsite))
     }
 }
---
+++ Tests/PrefabPlanner/createPrefab.test.js
@@ -0,0 +1,41 @@
+import { Selector, t } from 'testcafe';
+import VueSelector from 'testcafe-vue-selectors';
+import Create from "../../PageObjects/subHeader.po";
+import addItem from "../../PageObjects/AddItemPO.po";
+import Checklist from "../../PageObjects/ChecllistPO.po";
+import ImportItem from "../../PageObjects/ImportItemPO.po";
+
+const create = new Create();
+const item = new addItem();
+const checklist = new Checklist();
+const importItem = new ImportItem('planning');
+
+export default {
+    createPrefab: async (name) => {
+        await t
+            .click(Selector('[data-target="navbarBasicExample"].navbar-burger').find('span'))
+            .click(Selector('a').withText('PREFAB PLANNER'))
+            .click(Selector('[data-target="navbarBasicExample"].navbar-burger'))
+            .expect(Selector('[class^="button is-rounded image is-32x32 is-primary has-to"]').find('.icon').exists).ok()
+            .click(Selector('[class^="button is-rounded image is-32x32 is-primary has-to"]').find('.icon'))
+            .click(Selector('button').withText('Create'))
+        await create.creatPrefab(name, '8', '29')
+        for (let i = 0; i < 3; i++) {
+            await item.addPrefabItems((i + 1).toString(), 'Prefab item '.concat(i), '10', 'l-01', 'z-01', '123', '22', '23', 'item-note')
+        }
+        // await item.addPrefabItems('1', 'item-01',  '10', 'l-01','z-01', '123', '22', '23', 'item-note')
+        await importItem.itemImport('/home/access/TestCafeStudio/stage-manufacton-com/_uploads_/3 items 5.xlsx')
+        checklist.addprefabChecklist('checklist-01', '16')
+        await t
+            .click(Selector('.fas.fa-check'), {
+                modifiers: {
+                    ctrl: true
+                }
+            })
+            .wait(2000)
+            .expect(create.orderName.value).contains(name)
+            .expect(create.coordBy.value).contains("8")
+            .expect(create.prefabOnsite.value).contains("29")
+        await t.click(Selector('.fas.fa-check'));
+    },
+}
