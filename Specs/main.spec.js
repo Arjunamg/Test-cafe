e989e9d54fed6e79e89297f33c8cb2adcc74d7f0
SnehaGBhat24 <sneha.g.bhat515@gmail.com>
2019-05-29 13:14:00 +0530

    Inventory Import and file name change

--- Specs/main.spec.js
+++ Specs/main.spec.js
@@ -7,6 +7,7 @@ import orderName from './../Helpers/RandomNames';
 import BasicParams from './../Helpers/BasicParams';
 import GC_Role from './../Helpers/Roles';
 import editMfCard from '../Tests/ProductionManager/editManufacturingCard.tests';
+import invrntoryImport from '../Tests/InventoryManager/importInventoryItems.tests'
 
 
 
@@ -46,38 +47,44 @@ test('Material', async t => {
     3. Checklist for order*/
   await t.navigateTo('https://stage.manufacton.com/#/materials/preparation')
   await createMaterial.createMaterial(name);
-  await common.search(name);
+
+  await t.navigateTo('https://stage.manufacton.com/#/prefabs-list')
+  await createPrefab.createPrefab(name);
+
+  await t.navigateTo('https://stage.manufacton.com/#/im')
+  await invrntoryImport.importItems();
+  // await common.search(name);
   //Function for splitting the order
-  await common.split(`Split-of-${name}`);
-  await common.clearSearch();
-  await common.search(name);
-  //Function for Cloning the Order
-  await common.clone();
-  await common.search(`Clone Of ${name}`);
-  await common.clearSearch();
-  //Function for Combining Order
-  await common.combine(`${name}`,`Split-of-${name}`);
-  await common.search(`Clone Of ${name}`);
-  //Function For Removing the material order
-  await common.remove();
-  await common.clearSearch();
-  await common.search(name);
-  //Function for Moving the order To FM
-  await common.move('forward','FM');
-  await common.clearSearch();
-  await t.navigateTo('https://stage.manufacton.com/#/materials/qa');
-  await common.search(name);
-  //Function for Editing the order in FM
-  await edit.editCard(name);
-  await common.search(name);
-  //Function for Moving the order To Ordering
-  await common.move('forward','Ordering');
-  await t.navigateTo('https://stage.manufacton.com/#/materials/ordering');
-  await common.search(name);
-  //Function for Create Shipping 
-  await common.ship();
-  await t.navigateTo('https://stage.manufacton.com/#/scm/shipping/order-view');
-  await common.search(name);
+  // await common.split(`Split-of-${name}`);
+  // await common.clearSearch();
+  // await common.search(name);
+  // //Function for Cloning the Order
+  // await common.clone();
+  // await common.search(`Clone Of ${name}`);
+  // await common.clearSearch();
+  // //Function for Combining Order
+  // await common.combine(`${name}`,`Split-of-${name}`);
+  // await common.search(`Clone Of ${name}`);
+  // //Function For Removing the material order
+  // await common.remove();
+  // await common.clearSearch();
+  // await common.search(name);
+  // //Function for Moving the order To FM
+  // await common.move('forward','FM');
+  // await common.clearSearch();
+  // await t.navigateTo('https://stage.manufacton.com/#/materials/qa');
+  // await common.search(name);
+  // //Function for Editing the order in FM
+  // await edit.editCard(name);
+  // await common.search(name);
+  // //Function for Moving the order To Ordering
+  // await common.move('forward','Ordering');
+  // await t.navigateTo('https://stage.manufacton.com/#/materials/ordering');
+  // await common.search(name);
+  // //Function for Create Shipping 
+  // await common.ship();
+  // await t.navigateTo('https://stage.manufacton.com/#/scm/shipping/order-view');
+  // await common.search(name);
 })
 
 
